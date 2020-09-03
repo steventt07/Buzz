47a02ea85bdd0a2e3402af1bcec76d920e6e2a4e
Steven Tran <steven@Stevens-MacBook-Pro.local>
2020-09-03 18:11:19 -0500

    added more exampels

--- app/database/examples/data.sql
+++ app/database/examples/data.sql
@@ -13,6 +13,34 @@ INSERT INTO category_table(
 	)
 VALUES ('What''s happening?', 'steventt07', NOW());
 
+INSERT INTO category_table(
+        category_name,
+        owner_username,
+        date_created
+	)
+VALUES ('Deals', 'steventt07', NOW());
+
+INSERT INTO category_table(
+        category_name,
+        owner_username,
+        date_created
+	)
+VALUES ('Misc', 'steventt07', NOW());
+
+INSERT INTO category_table(
+        category_name,
+        owner_username,
+        date_created
+	)
+VALUES ('Happy Hour', 'steventt07', NOW());
+
+INSERT INTO category_table(
+        category_name,
+        owner_username,
+        date_created
+	)
+VALUES ('Recreation', 'steventt07', NOW());
+
 INSERT INTO post_table(
         username,
         category_name,
@@ -21,4 +49,114 @@ INSERT INTO post_table(
         zipcode,
         date_created
 	)
-VALUES ('steventt07', 'What''s happening?', 'DEALS!!!', 'My first post', '78703', NOW());
+VALUES ('steventt07', 'Deals', '', 'My first post', '78703', NOW());
+
+INSERT INTO post_table(
+        username,
+        category_name,
+		title,
+        content,
+        zipcode,
+        date_created
+	)
+VALUES ('steventt07', 'Deals', '', 'Huge jeans sale at target', '78703', NOW());
+
+INSERT INTO post_table(
+        username,
+        category_name,
+		title,
+        content,
+        zipcode,
+        date_created
+	)
+VALUES ('steventt07', 'Deals', 'Steak!!!', 'Steak is $7/lbs at HEB off Parmer', '78703', NOW());
+
+INSERT INTO post_table(
+        username,
+        category_name,
+		title,
+        content,
+        zipcode,
+        date_created
+	)
+VALUES ('steventt07', 'Deals', 'Burger', 'Happy at Hopdoddy, $5 for drinks', '78703', NOW());
+
+INSERT INTO post_table(
+        username,
+        category_name,
+		title,
+        content,
+        zipcode,
+        date_created
+	)
+VALUES ('steventt07', 'What''s happening?', 'DEALS!!!', 'Did yall see the crash on 35 and Parmer', '78703', NOW());
+
+INSERT INTO post_table(
+        username,
+        category_name,
+		title,
+        content,
+        zipcode,
+        date_created
+	)
+VALUES ('steventt07', 'What''s happening?', '!!!', 'Shooting downtown, stay safe ', '78703', NOW());
+
+INSERT INTO post_table(
+        username,
+        category_name,
+		title,
+        content,
+        zipcode,
+        date_created
+	)
+VALUES ('steventt07', 'What''s happening?', 'DEA!!', 'Epic rager on 6th', '78703', NOW());
+
+INSERT INTO post_table(
+        username,
+        category_name,
+		title,
+        content,
+        zipcode,
+        date_created
+	)
+VALUES ('steventt07', 'What''s happening?', '!!', 'There is a huge line at REI (Lamar)', '78703', NOW());
+
+INSERT INTO post_table(
+        username,
+        category_name,
+		title,
+        content,
+        zipcode,
+        date_created
+	)
+VALUES ('steventt07', 'Recreation', 'DEAL!', 'Volleyball tournament next friday “sign up link”', '78703', NOW());
+
+INSERT INTO post_table(
+        username,
+        category_name,
+		title,
+        content,
+        zipcode,
+        date_created
+	)
+VALUES ('steventt07', 'Recreation', 'DES!!!', 'Pick up basketball @rooftop ', '78703', NOW());
+
+INSERT INTO post_table(
+        username,
+        category_name,
+		title,
+        content,
+        zipcode,
+        date_created
+	)
+VALUES ('steventt07', 'Recreation', 'LS!!!', 'Paddle board place at Austin High School is open', NOW());
+
+INSERT INTO post_table(
+        username,
+        category_name,
+		title,
+        content,
+        zipcode,
+        date_created
+	)
+VALUES ('steventt07', 'Recreation', 'EALS!!!', 'Tennis courts at Austin High School is open', '78703', NOW());
