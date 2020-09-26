e16600c1e96a2dfc50000df75b0d072f56112c91
Steven Tran <steventt07@gmail.com>
2020-09-26 17:18:26 -0500

    added comment api endpoints

--- app/services/comment.py
+++ app/services/comment.py
@@ -3,12 +3,35 @@ import sys
 import psycopg2.extras
 from datetime import datetime
 from falcon.http_status import HTTPStatus
-from app.queries import QUERY_CHECK_CONNECTION, QUERY_INSERT_COMMENT
+from app.queries import QUERY_CHECK_CONNECTION, QUERY_INSERT_COMMENT, QUERY_GET_COMMENT
 
 class CommentService:
 	def __init__(self, service):
 		print('Initializing Comment Service...')
 		self.service = service
+	
+	def on_get(self, req, resp):
+		print('HTTP GET: /comment')
+		cursor = self.service.dbconnection.connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+		cursor.execute(QUERY_GET_COMMENT, (req.params['post_id'], ))
+		response = []
+		for record in cursor:
+			response.append(
+				{
+					'comment_id': record[0],
+					'username': record[1],
+					'content': record[2],
+					'date_created': str(record[3])
+					
+				}
+			)
+		
+		if len(response) == 0:
+			resp.status = falcon.HTTP_400
+		else:
+			resp.status = falcon.HTTP_200
+			resp.media = { 'comment': response}
+		cursor.close()
 		
 	def on_post(self, req, resp):
 		con = self.service.dbconnection.connection
